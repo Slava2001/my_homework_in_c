a997015da1600a1951994dd73d843b2952237797
slava2001 <slavakaplya20011501@gmail.com>
2018-11-20 08:12:06 +0300

    commit_2

--- Draw_pic.c
+++ Draw_pic.c
@@ -16,46 +16,47 @@ void Draw_f(int *x,int *y);
 
 int main()
 {
-while(1)
-{
-printf("Enter type:\n1 \\\n2 X\n3 []\n4 [*]\n5 exit\n");
-scanf("%d",&type);
-printf("Enter X:");
-scanf("%d",&x);
-x--;
-printf("Enter Y:");
-scanf("%d",&y);
-y--;
+	while(1)
+	{
+		printf("Enter type:\n1 \\\n2 X\n3 []\n4 [*]\n5 exit\n");
+		scanf("%d",&type);
+		printf("Enter X:");
+		scanf("%d",&x);
+		x--;
+		printf("Enter Y:");
+		scanf("%d",&y);
+		y--;
 
-for(int m=0;m<=sizeof(line);m++)
-line[m]=' ';
+		for(int m=0;m<=sizeof(line);m++)
+			line[m]=' ';
 
-switch(type)
-{
-case 1:
-Draw_d(&x,&y);
-break;
-case 2:
-Draw_x(&x,&y);
-break;
-case 3:
-Draw_k(&x,&y);
-break;
-case 4:
-Draw_f(&x,&y);
-break;
-default:
-return 0;
-}
+		switch(type)
+		{
+		case 1:
+			Draw_d(&x,&y);
+		break;
+		case 2:
+			Draw_x(&x,&y);
+		break;
+		case 3:
+			Draw_k(&x,&y);
+		break;
+		case 4:
+			Draw_f(&x,&y);
+		break;
+		default:
+			return 0;
+		}
 
-}
+	}
 return 0;
 }
 
 void Draw_f(int *x,int *y)
 {
 	for(int t=0;t<=*x;t++)
-	line[t]=background;
+		line[t]=background;
+
 	printf("%s\n",line);
 
 	for(int t=0;t<=*y-2;t++)
@@ -63,53 +64,52 @@ void Draw_f(int *x,int *y)
 		for(int i=0;i<*x;i++)
 		line[i]=pen;
 	
-	line[0]=background;
-	line[*x]=background;
-	printf("%s\n",line);
+		line[0]=background;
+		line[*x]=background;
+		printf("%s\n",line);
 	}
 
 	for(int t=0;t<=*x;t++)
-	line[t]=background;
+		line[t]=background;
+
 	printf("%s\n",line);
-
-
 }
 
 void Draw_k(int *x,int *y)
 {
-for(int t=0;t<=*x;t++)
-line[t]=background;
-printf("%s\n",line);
+	for(int t=0;t<=*x;t++)
+		line[t]=background;
 
-for(int t=0;t<=*x;t++)
-line[t]=pen;
-line[0]=background;
-line[*x]=background;
-printf("%s\n",line);
+	printf("%s\n",line);
 
-for(int i=0;i<=*y-4;i++) 
-{
-for(int d=0;d<=*x;d++)
-line[d]=background;
+	for(int t=0;t<=*x;t++)
+		line[t]=pen;
+	
+	line[0]=background;
+	line[*x]=background;
+	printf("%s\n",line);
 
-line[1]=pen;
-line[*x-1]=pen;
-printf("%s\n",line);
-}
+	for(int i=0;i<=*y-4;i++) 
+	{
+		for(int d=0;d<=*x;d++)
+			line[d]=background;
 
-for(int d=0;d<=*x;d++)
-line[d]=pen;
-line[0]=background;
-line[*x]=background;
-printf("%s\n",line);
+		line[1]=pen;
+		line[*x-1]=pen;
+		printf("%s\n",line);
+	}
 
-for(int t=0;t<=*x;t++)
-line[t]=background;
-printf("%s\n",line);
+	for(int d=0;d<=*x;d++)
+		line[d]=pen;
+	
+	line[0]=background;
+	line[*x]=background;
+	printf("%s\n",line);
 
-
-
-
+	for(int t=0;t<=*x;t++)
+		line[t]=background;
+	
+	printf("%s\n",line);
 }
 
 
@@ -117,14 +117,13 @@ void Draw_x(int *x,int *y)
 {
 	int s=10;
 
-
 	if(*x<*y)
 	{s=*x;}else{s=*y;}
 
 		for(int i=0;i<=s;i++)
 		{
 			for(int t=0;t<=*x;t++)
-			line[t]=background;
+				line[t]=background;
 			
 			line[i]=pen;
 			line[s-i]=pen;
@@ -133,16 +132,10 @@ void Draw_x(int *x,int *y)
 		for(int i=0;i<*y-s;i++)
 		{
 			for(int t=0;t<=*x;t++)
-			line[t]=background;
+				line[t]=background;
 
 			printf("%s\n",line);
 		}
-	
-
-
-
-
-
 }
 
 void Draw_d(int *x,int *y)
@@ -151,30 +144,30 @@ void Draw_d(int *x,int *y)
 	
 	if(*x<*y)
 	{
-	s=*x;
+		s=*x;
 	}else
 	{
-	s=*y;
+		s=*y;
 	}
 	
 
 	
-		for(int i=0;i<=s;i++)
-		{
-			for(int t=0;t<=*x;t++)
+	for(int i=0;i<=s;i++)
+	{
+		for(int t=0;t<=*x;t++)
 			line[t]=background;
 
-			line[i]=pen;
-			printf("%s\n",line);
-		}
-		for(int t=0;t<*y-s;t++)
-		{
-			for(int i=0;i<=*x;i++)
+		line[i]=pen;
+		printf("%s\n",line);
+	}
+
+	for(int t=0;t<*y-s;t++)
+	{
+		for(int i=0;i<=*x;i++)
 			line[i]=background;
 			
-			printf("%s\n",line);
-		}
-			
+		printf("%s\n",line);
+	}			
 }
 
 
